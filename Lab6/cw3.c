9b89ba81cc614a593e0add30b50d446f47898a9a
Your Name <you@example.com>
2022-03-15 10:21:43 +0530

    Lab6

--- Lab6/cw3.c
+++ Lab6/cw3.c
@@ -1,9 +1,13 @@
 #include<stdio.h>
 
 int main(){
-    int a,b,c;
-    printf("Enter three sides of the triangle");
-    scanf("%d%d%d",&a,&b,&c);
-    int h= a>b&&a>c;
+    int h,p,b;
+    printf("Enter the hypothesis ");
+    scanf("%d",&h);
+    printf("Enter the perpendicular ");
+    scanf("%d",&p);
+    printf("Enter the base ");
+    scanf("%d",&b);
+    printf("\nDo they satisfy triangle property? 1 for yes, 0 for no:%d",(h*h)==((p*p)+(b*b)));
     return 0;
 }
--- Lab6/cw5.c
+++ Lab6/cw5.c
@@ -2,33 +2,29 @@
 
 int main(){
 
-    float t=0.0,f,k;
+    float t=0.0,f;
     printf("Temp(C) \tTemp(F) \tTemp(K)\n");
 
     t+=20.0;
     f=(t*1.8)+32;
-    k=t+273;
-    printf("%f \t%f \t%f\n",t,f,k);
+
+    printf("%f \t%f \t%f\n",t,f,t+273);
 
     t+=20.0;
     f=(t*1.8)+32;
-    k=t+273;
-    printf("%f \t%f \t%f\n",t,f,k);
+    printf("%f \t%f \t%f\n",t,f,t+273);
 
     t+=20.0;
     f=(t*1.8)+32;
-    k=t+273;
-    printf("%f \t%f \t%f\n",t,f,k);
+    printf("%f \t%f \t%f\n",t,f,t+273);
 
     t+=20.0;
     f=(t*1.8)+32;
-    k=t+273;
-    printf("%f \t%f \t%f\n",t,f,k);
+    printf("%f \t%f \t%f\n",t,f,t+273);
 
     t+=20.0;
     f=(t*1.8)+32;
-    k=t+273;
-    printf("%f \t%f \t%f\n",t,f,k);
+    printf("%f \t%f \t%f\n",t,f,t+273);
 
     printf("\nComputation completed...");
     return 0;
